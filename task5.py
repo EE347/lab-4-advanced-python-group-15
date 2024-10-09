6cdd9ab8048c112420d6ec3fdef864fc8e88cd36
DaireShankey <d.shankey1@nuigalway.ie>
2024-10-09 15:27:28 +0100

    task 5 done

--- task5.py
+++ task5.py
@@ -1,2 +1,18 @@
 import csv
 
+filename = '/home/pi/ee347/lab-4-advanced-python-group-15/task5.csv'
+
+# Open the file in write mode
+with open(filename, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    while True:
+        name = input("Enter a name (or type 'quit' to stop): ")
+        if name.lower() == 'quit':
+            break
+        writer.writerow([name])
+
+# Open the file in read mode
+with open(filename, mode='r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row[0])
